7051df6d795eaa0fde74625ed62647a7e10211d5
Bobby Towers <porkostomus@mail.com>
2021-07-09 18:49:17 -0600

    spec probs 2-4

--- specs.clj
+++ specs.clj
@@ -2,6 +2,12 @@
   (:require [clojure.spec.alpha :as s]))
 
 (s/def ::problem-001 true?)
+(s/def ::problem-002 #(= 4 %))
+(s/def ::problem-003 #(= "HELLO WORLD" %))
+(s/def ::problem-004 #(= '(:a :b :c) %))
+
+;; Am I doing this right? Feels like I might be missing something.
+;; I expect this will get more interesting as we go on.
 
 (comment
   (s/valid? ::problem-001 false)
