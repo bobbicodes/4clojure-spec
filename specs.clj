1cf8481705ba34b169f2ea75e3a65807925b63b8
Bobby Towers <porkostomus@mail.com>
2021-07-10 12:51:56 -0600

    spec probs 5-8

--- specs.clj
+++ specs.clj
@@ -5,9 +5,16 @@
 (s/def ::problem-002 #(= 4 %))
 (s/def ::problem-003 #(= "HELLO WORLD" %))
 (s/def ::problem-004 #(= '(:a :b :c) %))
+(s/def ::problem-005 #(= '(1 2 3 4) %))
+(s/def ::problem-006 #(= '(:a :b :c) %))
+(s/def ::problem-007 #(= '[1 2 3 4] %))
+(s/def ::problem-008 #(= #{:c :b :d :a} %))
 
 ;; Am I doing this right? Feels like I might be missing something.
-;; I expect this will get more interesting as we go on.
+;; I could be making finer-grained specs and composing them,
+;; but would that not be pointless, considering that #3 need not just be
+;; a *string* and *capitalized*, it needs to satisfy `#(= "HELLO WORLD" %)`.
+;; I expect this to get more interesting as we go on.
 
 (comment
   (s/valid? ::problem-001 false)
